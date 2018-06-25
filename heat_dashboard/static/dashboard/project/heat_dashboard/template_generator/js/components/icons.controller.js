d448207b2b38189c24a8a51e0f7665b75b4661a3
Xinni Ge <xinni.ge1990@gmail.com>
2018-06-25 15:19:11 +0900

    Group resource icons by project
    Change-Id: I1266a931feff7bb969f0ac752a62cbe4e47f863f
    Story: 2002668
    Task: 22485

--- heat_dashboard/static/dashboard/project/heat_dashboard/template_generator/js/components/icons.controller.js
+++ heat_dashboard/static/dashboard/project/heat_dashboard/template_generator/js/components/icons.controller.js
@@ -9,8 +9,25 @@
         $scope.resource_admin = hotgenGlobals.get_node_admin();
         $scope.admin = hotgenGlobals.get_resource_options().auth.admin;
         $scope.basePath = basePath;
-    }]);
 
+        $scope.project_types = {};
+        for (var idx in $scope.resource_types){
+            var pidx = idx.split('__');
+            if (!pidx || pidx.length != 3){
+            continue
+        }
+        var level = pidx[0]+'::'+pidx[1]
+        if (! (level in $scope.project_types)){
+            $scope.project_types[level] = {}
+        }
+        $scope.project_types[level][idx] = $scope.resource_types[idx];
+        }
+        $scope.currentNavItem = Object.keys($scope.project_types)[0];
+        $scope.showIcon = function(){
+//            console.log($scope.currentNavItem)
+        };
+
+    }]);
 })();
 
 
